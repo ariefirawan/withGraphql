9cca055c10e45156775d75e0f1bfaf3944197479
ariefirawan <ariefirawan@users.noreply.github.com>
2020-02-18 15:06:42 +0700

    add image upload

--- controllers/feed.js
+++ controllers/feed.js
@@ -24,12 +24,18 @@ exports.createPost = (req, res, next) => {
         error.statusCode = 422;
         throw error;
     }
+    if (!req.file) {
+        const error = new Error('No Image provided')
+        error.statusCode = 422;
+        throw error;
+    }
+    const imageUrl = req.file.path.replace("\\" ,"/");
     const title = req.body.title;
     const content = req.body.content;
     const post = new Post({
         title: title, 
         content: content,
-        imageUrl: '/images/epkh.png',
+        imageUrl: imageUrl,
         creator: {
             name: 'sotoy'
         },
